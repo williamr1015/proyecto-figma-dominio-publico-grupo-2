29310468d85b0239ea84e990379f2bf58b5b145c
esteban perez <esteban.perez@alteru.co>
2021-06-23 13:26:33 -0500

    agregamos modal relatoria

--- assets/js/resultado.js
+++ assets/js/resultado.js
@@ -1,4 +1,9 @@
 function mostrarRelatoria(){
     const relatoria = document.getElementById('relatoria')
+    const cerrar = document.getElementById('cerrar')
     relatoria.classList.add('mostrar')
+}
+cerrar.addEventListener('click',this.cerrarModal)
+function cerrarModal(){
+    relatoria.classList.remove('mostrar')
 }
